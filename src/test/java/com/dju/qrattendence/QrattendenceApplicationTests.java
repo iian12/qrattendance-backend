e5575db67ff33ce342b65a5d084c03801677e30f
minchan <minchan3192@naver.com>
2026-01-14 18:45:40 +0900

    feat: start project, with JDK 17, Spring Boot 4.0.1

---
+++ src/test/java/com/dju/qrattendence/QrattendenceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dju.qrattendence;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QrattendenceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
