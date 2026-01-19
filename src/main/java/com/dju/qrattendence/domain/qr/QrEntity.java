5be0634c1193811c1c6fe2cd7b13f301bbbab1bc
minchan <minchan3192@naver.com>
2026-01-19 18:59:16 +0900

    ex: example

--- src/main/java/com/dju/qrattendence/domain/qr/QrEntity.java
+++ src/main/java/com/dju/qrattendence/domain/qr/QrEntity.java
@@ -1,4 +1,13 @@
 package com.dju.qrattendence.domain.qr;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
 public class QrEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
 }
