30dce5db22371b1aa55c19683ef06f8c0f5cde80
kotamimi <kayla.oh531@gmail.com>
2026-01-19 19:58:06 +0900

    feat: add attendance entity

---
+++ src/main/java/com/dju/qrattendence/domain/attendance/AttendanceRepository.java
@@ -0,0 +1,7 @@
+package com.dju.qrattendence.domain.attendance;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface AttendanceRepository
+        extends JpaRepository<AttendanceEntity, Long> {
+}
